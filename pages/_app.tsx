e826aabeaca4350e871321a62019b009b0876476
Shreyaschorge <shreyaschorge81@gmail.com>
2023-11-10 00:33:47 +0530

    Add minimal reproducible example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,51 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import App, { AppProps, AppContext, AppInitialProps } from 'next/app';
+import styles from '@/styles/App.module.css';
+import { useEffect, useState } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type AppAdditionalProps = { host: string };
+
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps & AppAdditionalProps) {
+  const [host, setHost] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (pageProps.host) {
+      // Log host on client
+      console.log('host: ', pageProps.host);
+      setHost(host);
+    }
+  }, [pageProps.host]);
+
+  return (
+    <>
+      {host ? (
+        <main className={`${styles.main}`}>
+          <Component {...pageProps} />
+          <h1>host from props in _app.tsx : {host} </h1>
+        </main>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
+
+MyApp.getInitialProps = async (
+  context: AppContext
+): Promise<AppAdditionalProps & AppInitialProps> => {
+  const ctx = await App.getInitialProps(context);
+
+  const host = context.ctx.req?.headers.host;
+
+  console.log('host: ', host);
+
+  const pageProps = {
+    ...ctx.pageProps,
+    host: context.ctx.req?.headers.host,
+  };
+
+  return { ...ctx, pageProps } as AppAdditionalProps & AppInitialProps;
+};
